843dd7fbf7b66f0fecb72abaf5eb1b0ef02998f2
Arief <ariefstwn3@gmail.com>
2021-11-08 12:43:26 +0700

    update login

--- app/src/main/java/com/example/ululunews/Register_activity.java
+++ app/src/main/java/com/example/ululunews/Register_activity.java
@@ -1,2 +1,25 @@
-package com.example.ululunews;public class Register_activity {
+package com.example.ululunews;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register_activity extends AppCompatActivity {
+
+    EditText et_username, et_email, et_password;
+    Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        et_username = findViewById(R.id.tf_username);
+        et_email = findViewById(R.id.tf_email);
+        et_password = findViewById(R.id.tf_password);
+        btn_register = findViewById(R.id.btn_register);
+
+    }
 }
